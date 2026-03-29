22c9671629e2e324e1690d9e63531131dc11f266
Reshav Pan <pansanjay209@gmail.com>
2026-03-29 20:15:03 +0530

    Removed the spaces in the echo command

--- quickpush.sh
+++ quickpush.sh
@@ -3,16 +3,16 @@
 #updated version
 
 if [ -z "$*" ]; then
-   echo "  Failed : no commit message"
+   echo "Failed : no commit message"
       exit 1
 fi
 
-git add . > /dev/null 2>&1 || { echo "  git add failed"; exit 1; }
-git commit -m "$*" > /dev/null 2>&1 || { echo "  git commit failed"; exit 1; }
+git add . > /dev/null 2>&1 || { echo "git add failed"; exit 1; }
+git commit -m "$*" > /dev/null 2>&1 || { echo "git commit failed"; exit 1; }
 
 branch=$(git branch --show-current)
-git push origin "$branch" > /dev/null 2>&1 || { echo "  git push failed"; exit 1; }
+git push origin "$branch" > /dev/null 2>&1 || { echo "git push failed"; exit 1; }
 
 remote_branch=$(git for-each-ref --format='%(refname:short)' refs/remotes/origin)
 
-echo "  Succesfully pushed form $branch to $remote_branch"
+echo "Succesfully pushed form $branch to $remote_branch"
